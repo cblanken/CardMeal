db749a5822f1241f3e2e944cbb354eb2d032b024
Cameron <cblanken9@yahoo.com>
2019-02-03 15:46:29 -0500

    Updated navbar icons.

--- app/src/main/java/com/example/cardmeal/MainActivity.java
+++ app/src/main/java/com/example/cardmeal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cardmeal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,7 +13,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -21,15 +21,19 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_search:
                     mTextMessage.setText(getString(R.string.title_search));
+                    gotoSearch(item);
                     return true;
                 case R.id.navigation_map:
                     mTextMessage.setText(getString(R.string.title_map));
+                    gotoMap(item);
                     return true;
                 case R.id.navigation_media:
                     mTextMessage.setText(getString(R.string.title_media));
+                    gotoMedia(item);
                     return true;
                 case R.id.navigation_settings:
                     mTextMessage.setText(getString(R.string.title_settings));
+                    gotoSettings(item);
                     return true;
 
             }
@@ -47,18 +51,24 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    public void gotoSearch(View view) {
-
+    public void gotoSearch(MenuItem item) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    public void gotoMap(View view) {
+    public void gotoMap(MenuItem item) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 
-    public void gotoMedia(View view) {
+    public void gotoMedia(MenuItem item) {
+        Intent intent = new Intent(this, Media.class);
+        startActivity(intent);
     }
 
-    public void gotoSettings(View view) {
-
+    public void gotoSettings(MenuItem item) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 
 }
