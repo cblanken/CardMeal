bfa63a0782a996b7bff32db761dc7fb3cac8727e
Cameron <cblanken9@yahoo.com>
2019-04-16 10:28:52 -0400

    Menu viewer setup to allow user to choose pdf reader instead of launching browser by default.

--- app/src/main/java/com/example/cardmeal/RestaurantRecyclerViewAdapter.java
+++ app/src/main/java/com/example/cardmeal/RestaurantRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.cardmeal;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -16,12 +17,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.LinkedList;
 import android.net.Uri;
+import android.webkit.WebView;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
+import com.google.firebase.database.core.Constants;
 
 public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<RestaurantRecyclerViewAdapter.RestaurantViewHolder> {
 
@@ -109,15 +112,20 @@ public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<Restaura
         holder.menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // TODO: implement WebView for menus?
+                // WebView webView = (WebView) ((Activity) context).findViewById(R.id.menuWebView);
+                // webView.getSettings().setJavaScriptEnabled(true);
+                // webView.loadUrl(mCurrent.menu);
+                // webView.setVisibility(View.VISIBLE);
+
                 String mimeType = "application/pdf";
                 Uri menuPdf = Uri.parse(mCurrent.menu);
-                Intent intent = new Intent(Intent.ACTION_VIEW, menuPdf);
+                Intent intent = new Intent(Intent.ACTION_VIEW).setDataAndType(menuPdf, mimeType);
 
-                if(intent.resolveActivity(context.getPackageManager()) != null) {
-                    context.startActivity(intent);
-                } else {
-                    Log.d("MenuPdf", "No pdf reader found on device!");
-                }
+                Intent chooser = Intent.createChooser(intent, context.getResources().getString(R.string.chooser_title));
+                chooser.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                context.startActivity(chooser);
             }
         });
 
